dceaf42b8a4da1c0f93936bc5a13bba5e8681a49
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2022-10-25 15:38:19 +0200

    I have completed the desktop/tablet and mobile version for the first page

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <title>Circle Solutions</title>
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <script src="index.js" defer type="text/javascript"></script>
+</head>
+
+<body>
+
+  <header>
+    <div class="nav-wrapper">
+      <div class="left-side">
+         <div>
+           <img src="img/logo.png" title="logo" alt="logo" width="195px" height="65px">
+         </div>
+       </div>
+
+      <ul id="navigation" data-visible="false" class="navigation">
+          <li><a href="index.php" class="style active">Home</a></li>
+          <li><a href="reviews.html" class="style">Reviews</a></li>
+          <li><a href="values.html" class="style">Values</a></li>
+          <li><a href="contact.html" class="style">Contact</a></li>
+          <li><a href="faq.html" class="style">FAQ</a></li>
+          <li><a href="Packages.html"><button class="button button1">Our Packages</button></a></li>
+        </ul>
+
+        <button class="mobile-nav-toggle" aria-controls="navigation" aria-expanded="false"><span class="sr-only"></span>
+        </button>
+  </header>
+  <sect1>
+
+    <div class="content1">
+        <div class="pad">
+          <h1>We provide<br>software solutions<br>to businesses<br>globally</h1>
+          <a href="Packages.html"><button class="button button2">View Packages</button></a> 
+        </div>
+
+        <div>
+           <img src="img/globe.png" width="600px">
+         </div>
+      
+    </div>
+    
+  </sect1>
+
+
+  <sect2>
+    <div class="content2">
+      <div >
+           <img src="img/plans.png" width="500px">
+         </div>
+
+        <div class="pad2">
+          <h1>We connect the dot to develop software that fit like a glove</h1> 
+        </div>
+    </div>
+
+      <hr class="style">
+  </sect2>
+<sect3>
+	<div class="content3">
+    <div class="trusted">
+        <h2>Trusted Partners</h2>
+      </div>
+
+      <div class="img-container">
+  <img class="items" src="img/esdal.png">
+    <img class="items" src="img/carmel.png">
+	  <img class="items" src="img/hondsrug.png">
+	    <img class="items" src="img/nhl.png">
+		  <img class="items" src="img/terra.png">
+		    <img class="items" src="img/dc.png">
+  </div>
+</div>
+  </sect3>
+
+  <footer>
+    <div class="footer-wrap">
+		<div class="subscribe">
+        <h2>Newsletter</h2>
+        <p class="merge">Want to know what we’re up to? <br> Sign up for the newsletter and join our tribe</p>
+        <button class="open-button" onclick="openForm()">Subscribe</button>
+        </div>
+		
+		
+		<div class="footer-right">
+		<div class="footer-social">
+  <img class="items2" src="img/instagram.png">
+    <img class="items2" src="img/facebook.png">
+	  <img class="items2" src="img/twitter.png">
+	  </div>
+	  <p> &#169; 2022, All rights reserved </p>
+  </div>
+		</div>
+	</div>
+  </footer>
+
+</body>
+
+</html>
