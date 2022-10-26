a8d475a4f19663b499ddbad3bc7b8348366997fe
CadarIustin <iustin.cadar@student.nhlstenden.com>
2022-10-26 16:56:42 +0200

    first try to create the faq page

---
+++ FAQ.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+    <html lang="en">
+<head>
+    <link href="style.css" rel="stylesheet" type=text/css>
+    <script type="text/javascript" src="email.js"></script>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="containerimg">
+            <a href=""><img src="img/logo.png" alt="img" height="65px", width="195px"></a>
+        </div>
+        <a href="" class="homelink">Home</a>
+        <a href="" class="revlink">Reviews</a>
+        <a href="" class="valuelink">Values</a>
+        <a href="" class="contlink">Contact</a>
+        <a href="" class="faqlink">FAQ</a>
+        <a href="" class="button">Our Packages</a>
+	     </div>
+		 
+		 <div>
+		 <p class="Isten" style="font-size:275%;" ><strong> Frequently Asked Questions</strong></p>
+		 </div>
+		 
+	  <div id="grid">
+			
+		 <div class="faq-column1">
+		 
+			<div class="item"><h2 class="h3"><strong>What is an intranet?</strong></h2>
+			
+			     <p class="p1">Intranet means a closed network that resides on a local network. An intranet
+				 acts like a website that only authorized users can access and is generally used by
+				 organizations who wish to share knowledge, internal communications, and information with their colleagues.</p></div>
+			
+			<div class="item"><h2 class="h2"><strong>What is the diference betwen the intrenet and an intranet?</strong></h3>
+			     
+				 <p class="p1">The main difference between an intranet and the internet is that the former 
+				 is a closed network, and the latter is a public network. In short, the internet is for all;
+				 a company intranet is for a select group of people.</p></div>
+				 
+		    <div class="item"><h2 class="h2"><strong>What is an intranet and an extranet?</strong></h2>
+			    
+				<p class="p1">An intranet is a closed network designed to allow an organization to share
+				information and corporate communication among its employees. An extranet is 
+				similar to an intranet, but allows third-party entry, so vendors or partners can 
+				experience full or selected access.</p></div>
+				
+			<div class="item"><h2 class="h2"><strong>Can an intranet work without the internet?</strong></h2>
+			    
+				<p class="p1">Former on-premise company intranets could operate without internet.
+				But for a wide range of reasons, the majority of company intranet applications
+				are now cloud-based, so an internet connection is necessary.</p></div>	
+				
+			<div class="item"><h2 class="h2"><strong>Can an intranet be accesed remotely?</strong></h2>
+			    
+				<p class="p1">Yes, an intranet can be accessed on a range of devices, regardless of 
+				location – all that is required is an internet connection. This is ideal for 
+				any organization with dispersed or front-line employees who need communication 
+				tools and access to organizational information wherever they are.</p></div>	
+				
+		 </div>
+		 
+		  <div class="faq-column2">
+		  
+		 
+		  
+		  <div class="item2"><h2 class="h3"><strong>What are intranet sites?</strong></h2>
+		       
+			   <p class="p1">Intranet sites are closed internal networks for authorized members
+				of an organization to share information. They act as a company news channel,
+				an internal communications tool, and a collaboration tool.<p/></div>
+				
+		   <div class="item2"><h2 class="h2"><strong>What are intranet applications?</strong></h2>
+		       
+			   <p class="p1">Intranet applications are software within an intranet like document publishing,
+				communication tools, broadcasts, etc. These applications allow the user to do a range 
+				of activities, for example: publish a blog, connect with a colleague, or run employee surveys.</p></div>
+				
+			<div class="item2"><h2 class="h2"><strong>What are intranet applications?</strong></h2>
+			   
+			   <p class="p1">An intranet portal is the access point to the user’s company intranet. 
+				It is on the intranet portal where the user can access intranet applications as well as
+				external apps, and information and knowledge that is stored within the intranet itself.</p></div>
+				
+			<div class="item2"><h2 class="h2"><strong>What does an intranet do?</strong></h2>
+			   
+			   <p class="p1">Offering all types of organizational solutions, intranet software
+				 can improve employee engagement, aid communication, and increase collaboration.
+				 Modern intranets are now mostly cloud-based which allows global organizations to 
+				 connect, share knowledge, and broadcast news quickly and easily.<p></div>
+				
+	      </div>
+	  
+	</div>
+		 
+		  
+</header>
+
+<footer>
+    <div>
+        <h2>Newsletter</h2>
+        <p>Want to know what we’re up to?</p>
+        <p>Sign up for the newsletter and join our tribe</p>
+        <button class="open-button" onclick="openForm()">Subscribe</button>
+        <div class="form-popup" id="myForm">
+            <form action="#" class="form-container">
+                <label for="name"><b>Name</b></label>
+                <input type="text" placeholder=" Your name" name="name" required>
+                <label for="email"><b>E-mail</b></label>
+                <input type="text" placeholder=" Your E-mail" name="email" required>
+                <button type="submit" class="btn">Send</button>
+                <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+            </form>
+        </div>
+    </div>
+    <div>
+        <a href="https://www.instagram.com/"><img src="img/img_1.png" alt="img" height="49.33px" width="50.16px" style="margin-right: 20px"></a>
+        <a href="https://www.facebook.com/"><img src="img/img_2.png" alt="img" height="49.33px" width="50.16px" style="margin-right: 20px"></a>
+        <a href="https://twitter.com/"><img src="img/img_3.png" alt="img" height="49.33px" width="50.16px"></a>
+        <p style="margin-top: 20px; font-size: 20px;", >2022. All right reserved</p>
+    </div>
+</footer>
+</body>
+</html>
