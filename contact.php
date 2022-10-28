71f6014b7536d16a0a2f21559b45c50c2d436085
DenissSamcuks <111962469+DenissSamcuks@users.noreply.github.com>
2022-10-28 23:22:17 +0200

    Add files via upload
    contact us page. and css .

--- contact.php
+++ contact.php
@@ -2,70 +2,107 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="page2.css" rel="stylesheet" type=text/css>
+    <script type="text/javascript" src="email.js"></script>
+    <script src="main.js" defer type="text/javascript"></script>
+    <title>Circle Solutions</title>
 </head>
-	<head>
-		<title>My first form</title>
-		<meta charset="utf-8">
-		<link href="css/style.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<?php
-		if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $name = filter_input(INPUT_POST, "firstname");
-                $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-                $subject = filter_input(INPUT_POST, "subject");
-                $message = filter_input(INPUT_POST, "message");
-                $newsletter = filter_input(INPUT_POST, "subscribe");
-				$rick = $_POST["rick"];
-				$pass = filter_input(INPUT_POST, "password");
+<body>
+<header>
+    <div class="nav-wrapper">
+        <div class="left-side">
+            <div>
+                <img src="img/logo.png" title="logo" alt="logo" width="195px" height="65px">
+            </div>
+        </div>
 
-           		var_dump($rick);
-            }
-		?>
-		<h1>This is our first form</h1>
-			
-		<form name="contact" action="contact.php" method="POST">
-			<div>
-				<label for="firstname">Firstname</label> 
-				<input type="text" name="firstname" id="firstname" placeholder="Firstname">
-			</div>
-			<div>
-				<label for="email"><span class="required">*</span>Email</label>
-				<input type="text" name="email" id="email" required placeholder="Email">
-			</div>
-			<div>
-				<label for="subject">Subject</label>
-				<select name="subject" id="subject">
-					<option>Complaint</option>
-					<option selected>Suggestion</option>
-					<option>Question</option>
-				</select>
-			</div>
-			<div>
-				<textarea name="message" placeholder="your message" class="textarea"></textarea>
-			</div>
-			<div>
-				do you want to subscribe?
-			    <input type="radio" name="subscribe" value="yes" checked>Yes
-				<input type="radio" name="subscribe" value="no">No
-			</div>
-			<div>
-				Never gonna?
-			    <input type="checkbox" name="rick[]" value="give you up" checked>Give you up
-				<input type="checkbox" name="rick[]" value="let you down">Let you down
-			</div>
-			<div>
-				<label for="password">Password</label>
-				<input type="password" name="password" id="password"> 
-			</div>
-			<div>
-				<input type="submit" name="submit" value="Send Form">
-				<input type="reset">
-			</div>
-		</form>
-	</body>
+        <ul id="navigation" data-visible="false" class="navigation">
+            <li><a href="index.html" class="style">Home</a></li>
+            <li><a href="reviews.html" class="style">Reviews</a></li>
+            <li><a href="values.html" class="style">Values</a></li>
+            <li><a href="contact.php" class="style">Contact</a></li>
+            <li><a href="faq.html" class="style">FAQ</a></li>
+            <li><a href="Packages.php"><button class="button button1">Our Packages</button></a></li>
+        </ul>
+
+        <button class="mobile-nav-toggle" aria-controls="navigation" aria-expanded="false"><span class="sr-only"></span>
+        </button>
+</header>
+
+<div class="pusto">
+</div>
+
+<div class="We’d">
+    <h1 class="We’dh1">We’d Love to hear from you</h1>
+    <p class="psize">if you have a question about features, trials, pricing, need a demo or anything else, our team </p>
+    <p class="psize">is ready to answer all your questions </p>
+    <div class="getbut">
+        GET IN TOUCH
+    </div>
+</div>
+
+<div id="main">
+    <div class="Contactus">
+        <h2 style="font-size: 30px">Contact Us</h2>
+    </div>
+
+
+    <div class="needtocallus">
+        <div class="leftbox">
+            <h2 class="needtocallush2">Need to call us?</h2>
+            <div class="needtocallusp">
+                <p>Are there any problems or</p>
+                <p>questions you have? Don’t</p>
+                <p>be afraid to call us on this</p>
+                number:
+            </div>
+            <img src="whatsapp.png" alt="img" height="33px" width="33px" style="margin-top: 20px"></a><br>
+            <p style="margin-bottom: 10px"  >+31 6 0000 0000</p>
+        </div>
+
+        <div class="rightbox">
+            <h2 class="needtocallush2">Contact us on
+                <p>social media</p></h2>
+            <div class="pic">
+                <a href="https://www.instagram.com/"><img src="insta.png" alt="img" height="60px" width="60px" style="margin-right: 20px"></a>
+                <a href="https://www.facebook.com/"><img src="facebook.png" alt="img" height="60px" width="60px"></a>
+                <a href="https://twitter.com/"><img src="twitter.png" alt="img" height="60px" width="60px" style="margin-left: 20px"></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="footer-wrap">
+        <div class="subscribe">
+            <h2>Newsletter</h2>
+            <p class="merge">Want to know what we’re up to? <br> Sign up for the newsletter and join our tribe</p>
+            <button class="open-button" onclick="openForm()">Subscribe</button>
+            <div class="form-popup" id="myForm">
+                <form action="#" class="form-container">
+                    <label for="name"><b>Name</b></label>
+                    <input type="text" placeholder=" Your name" name="name" required>
+
+                    <label for="email"><b>E-mail</b></label>
+                    <input type="text" placeholder=" Your E-mail" name="email" required>
+
+                    <button type="submit" class="btn">Send</button>
+                    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                </form>
+            </div>
+        </div>
+
+
+        <div class="footer-right">
+            <div class="footer-social">
+                <a href="https://www.instagram.com/"><img class="items2" src="img/instagram.png"></a>
+                <a href="https://www.facebook.com/"><img class="items2" src="img/facebook.png"></a>
+                <a href="https://www.twitter.com/"><img class="items2" src="img/twitter.png"></a>
+            </div>
+            <p> &#169; 2022, All rights reserved </p>
+        </div>
+    </div>
+    </div>
+</footer>
+</body>
 </html>
